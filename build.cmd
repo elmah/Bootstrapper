034f2541b610e91383d04c56adf0f65f88fb87cd
Atif Aziz <code@raboof.com>
2016-02-05 19:45:53 +0100

    Lock MSBuild version to MS Build Tools 2015

--- build.cmd
+++ build.cmd
@@ -1,6 +1,16 @@
 @echo off
 setlocal
 pushd "%~dp0"
+if "%PROCESSOR_ARCHITECTURE%"=="x86" set MSBUILD=%ProgramFiles%
+if defined ProgramFiles(x86) set MSBUILD=%ProgramFiles(x86)%
+set MSBUILD=%MSBUILD%\MSBuild\14.0\bin\msbuild
+if not exist "%MSBUILD%" (
+    echo Microsoft Build Tools 2015 does not appear to be installed on this
+    echo machine, which is required to build the solution. You can install
+    echo it from the URL below and then try building again:
+    echo https://www.microsoft.com/en-us/download/detailscd .aspx?id=48159
+    exit /b 1
+)
 nuget restore              ^
  && call :build 40 Debug   ^
  && call :build 40 Release ^
@@ -10,5 +20,6 @@ popd
 goto :EOF
 
 :build
-msbuild /p:Configuration=NET%1-%2 /v:m %3 %4 %5 %6 %7 %8 %9
+setlocal
+"%MSBUILD%" /p:Configuration=NET%1-%2 /v:m %3 %4 %5 %6 %7 %8 %9
 goto :EOF
