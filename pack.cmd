865f44bfffe3759d27bb684806b162cf5683e80d
Atif Aziz <code@raboof.com>
2017-09-26 09:11:36 +0200

    Don't hardcode the package version suffix

--- pack.cmd
+++ pack.cmd
@@ -1,4 +1,11 @@
 @echo off
 pushd "%~dp0"
-call build && nuget pack Elmah.Bootstrapper.nuspec -Symbol
+call :main %*
 popd
+goto :EOF
+
+:main
+setlocal
+if not "%1"=="" set NUGET_PACK_ARGS=%NUGET_PACK_ARGS% -Properties VersionSuffix=-%1
+call build && nuget pack Elmah.Bootstrapper.nuspec %NUGET_PACK_ARGS%
+goto :EOF
