ac14ac708065472c5f5b43e373a87e1038a93ef8
Atif Aziz <code@raboof.com>
2017-09-26 09:12:26 +0200

    Output packages to their own dir

--- pack.cmd
+++ pack.cmd
@@ -6,6 +6,8 @@ goto :EOF
 
 :main
 setlocal
+if not exist dist md dist || exit /b 1
+set NUGET_PACK_ARGS=-Symbol -OutputDirectory dist
 if not "%1"=="" set NUGET_PACK_ARGS=%NUGET_PACK_ARGS% -Properties VersionSuffix=-%1
 call build && nuget pack Elmah.Bootstrapper.nuspec %NUGET_PACK_ARGS%
 goto :EOF
